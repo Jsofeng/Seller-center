0ba28db0d72b8e70e1f527b4289660a7f12e89d7
Jonathan <jonathan.jf.zhao@gmail.com>
2025-11-15 05:32:18 +0000

    fixed syntax error

--- python-product-AIBot/scraper/alibaba_scraper.py
+++ python-product-AIBot/scraper/alibaba_scraper.py
@@ -2,36 +2,34 @@ from playwright.sync_api import sync_playwright
 
 class AlibabaScraper:
 
-	def run(self, query, max_result=5):
-            with sync_playwright() as p:
-		browser = p.chromium.launch(headless=True) #browser runs in the background, no UI shown
-		page = browser.new_page()
+    def run(self, query, max_results=5):
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)  # browser runs in the background, no UI shown
+            page = browser.new_page()
 
-		url = f"https://cargoplus.site/search?searchText={query}"
-		page.goto(url)
-		page.wait_for_timeout(3000)
+            url = f"https://www.alibaba.com/trade/search?SearchText={query}"
+            page.goto(url)
+            page.wait_for_timeout(3000)
 
-		items = page.locator(".organic-gallery-offer-card").all() #finds & returns all HTML elements that represent individual product cards on the page.
+            items = page.locator(".organic-gallery-offer-card").all()  # finds & returns all HTML elements that represent individual product cards on the page.
 
-		products = []
-		for item in items[:max_results]:
-		   try:
+            products = []
+            for item in items[:max_results]:
+                try:
+                    title = item.locator(".element-title-normal_content").inner_text()  # .locator().inner_text() to extract title and price.
+                except:
+                    title = ""
 
-			title = item.locator[".element-title-normal_content").inner_text() #.locator().inner_text() to extract title and price.
-		   except:
-			title = ""
+                try:
+                    price = item.locator(".element-offer-price-normal_price").inner_text()
+                except:
+                    price = ""
 
-		   try:
-			price = item.locator(".element-offer-price-normal_price").inner_text()
-		   except:
-			price = ""
+                products.append({
+                    "title": title.strip(),
+                    "price": price.strip(),
+                    "source": "Alibaba"
+                })
 
-		   products.append({
-			"title": title.strip()
-			"price": price.strip()
-			"source": "Alibaba"
-
-		   })
-
-		   browser.close()
-		   return products #returns a list of all product in the dictionary
+            browser.close()
+            return products  # returns a list of all product in the dictionary
